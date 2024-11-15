19e79016e54e1d7b9cb1ed75e397c4c744cccdcc
RikuLeinonenVuoksi <88713943+RikuTheKiller@users.noreply.github.com>
2024-11-15 10:42:12 +0200

    buncha shit

---
+++ Assets/Scripts/Tiles/SimpleTile.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+namespace Scarcity
+{
+    /// <summary>
+    /// A very simple tile with a sprite and some settings.
+    /// </summary>
+    [CreateAssetMenu(fileName = "Simple Tile", menuName = "2D/Tiles/Simple Tile", order = 10)]
+    public class SimpleTile : BasicTile
+    {
+        public Sprite sprite;
+
+        public Tile.ColliderType colliderType = Tile.ColliderType.Grid;
+
+        public override void GetTileData(Vector3Int position, ITilemap tilemap, ref TileData tileData)
+        {
+            base.GetTileData(position, tilemap, ref tileData);
+
+            tileData.sprite = sprite;
+            tileData.colliderType = colliderType;
+        }
+    }
+}
